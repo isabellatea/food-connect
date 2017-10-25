d6c68ad561b158b6f8520e286bc4ea69f1b325e6
Isabella Tea <tea.isabella@gmail.com>
2017-10-25 16:16:51 -0700

    react-bootstrap integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Thumbnail, Jumbotron, Button } from 'react-bootstrap';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse fixedTop>
+      <Navbar />
+      <header className='Header'>
+        <Jumbotron className='Jumbotron'>
           <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
+            <h1>Food Connect</h1>
+            <p>Connecting organizations, volunteers, and those in need</p>
+
           </Grid>
         </Jumbotron>
+      </header>
+      <section className='About'>
+        <Grid>
+          <Row>
+            <Col xs={6} md={4}>
+              <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                <h3>Organizations</h3>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">Button</Button>&nbsp;
+                  <Button bsStyle="default">Button</Button>
+                </p>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={4}>
+              <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                <h3>Volunteers</h3>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">Button</Button>&nbsp;
+                  <Button bsStyle="default">Button</Button>
+                </p>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={4}>
+              <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                <h3>Education</h3>
+                <p>Description</p>
+                <p>
+                  <Button bsStyle="primary">Button</Button>&nbsp;
+                  <Button bsStyle="default">Button</Button>
+                </p>
+              </Thumbnail>
+            </Col>
+          </Row>
+        </Grid>
+      </section>
+      <footer className='Footer'>
+        Footer Here
+      </footer>
       </div>
     );
   }
