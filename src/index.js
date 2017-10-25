fd40ab5117e680a128e749e1554c25be049e301c
Isabella Tea <tea.isabella@gmail.com>
2017-10-25 15:26:54 -0700

    initial file structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
-import App from './App';
+import './css/styles.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
